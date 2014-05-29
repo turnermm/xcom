1e72735adb23d5dc32168fe166717276aafe03b5
Myron Turner <turnermm02@shaw.ca>
2014-05-29 16:59:02 -0500

    documentation, lang string updates

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -8,7 +8,7 @@
 
 // for admin plugins, the menu prompt to be displayed in the admin menu
 // if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu'] = 'XMLRPC Dashboard'; 
+$lang['menu'] = 'xcom XMLRPC Dashboard'; 
 $lang['xmlrpc_fns'] ='For a complete listing with full details see';
 $lang['close'] = 'close';
 $lang['functions'] =  'Query types';
