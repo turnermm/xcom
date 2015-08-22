a816ac6f37df1a477d95630bc95178ff40ca9b65
Padhie <vivien.nalenz@web.de>
2015-08-22 11:20:04 +0200

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Padhie <vivien.nalenz@web.de>
+ */
+$lang['close']                 = 'Schließen';
+$lang['select']                = 'Query auswählen';
+$lang['send']                  = 'Query senden';
+$lang['user']                  = 'Benutzername';
+$lang['pwd']                   = 'Passwort';
+$lang['invalid']               = 'Ungültige Eingabe erkannt!';
+$lang['pageid']                = 'ID';
+$lang['pageid_title']          = 'Seite, Mediadatei oder Namespace';
+$lang['options']               = 'Optionen';
+$lang['howto']                 = 'Einführungen';
+$lang['results']               = 'Seite Aufrufe';
+$lang['editable']              = 'Bearbeiten';
+$lang['pre']                   = 'Seite Code';
+$lang['html']                  = 'Heite HTML';
+$lang['results_tip']           = 'Öffne/Schließe Ausgabefenster';
+$lang['clear']                 = 'Leeren';
+$lang['query']                 = 'Query';
+$lang['noconnection']          = 'Keine Verbindung. Haben Sie die API-Konfigurationen gesetzt?';
+$lang['hideallviews']          = 'Verstecke Aufrufe';
+$lang['pwdview']               = 'Seite Passwort';
+$lang['pwdhide']               = 'Verstecke Passwort';
+$lang['localpwd']              = 'Lokales passwort';
+$lang['locuser']               = 'Lokaler Benutzername';
+$lang['locsave']               = 'Lokaler Speicher';
+$lang['sel_sites']             = 'Entfernte Seite';
+$lang['success']               = 'Erfolgreicher Login';
+$lang['nopage']                = 'Kein Seitennamen übergeben';
+$lang['notonremote']           = 'Nicht auf dem Server gefunden:';
+$lang['nomedia']               = 'Keine Mediadatei gefunden';
+$lang['imported']              = 'Wichtig:';
+$lang['logoff']                = 'Anmeldung ausgeschaltet';
+$lang['nologoff']              = 'Abmeldefunktion nicht erreichbar';
+$lang['localdw']               = 'Lokale Dokuwiki Version =';
+$lang['remotedw']              = 'Entfernte Dokuwiki Version = ';
+$lang['msave']                 = 'Speicher lokales Wiki in: ';
+$lang['fsize']                 = 'Dateigröße: ';
+$lang['savelocalfile']         = 'Klicken OK zum speichern der Datei im lokalen Wiki:';
