2f5e5ad2d7d2f8f08c065ee3c5f60af44e749edd
Myron Turner <turnermm02@shaw.ca>
2021-03-18 20:53:26 -0500

    redstores debug option

--- scripts/xml.php
+++ scripts/xml.php
@@ -9,8 +9,14 @@ $url = rtrim ($credentials->url,'/') . '/';
 
 
 $params = json_decode($_REQUEST['params']);
-$client = xcom_connect($url,$credentials->user,$credentials->pwd ,0);
+if($_REQUEST['debug'] == 'false'){
+    $debug = 0;    
+}   
+elseif ($_REQUEST['debug'] == 'true') {
+    $debug = 1;
+}
 
+$client = xcom_connect($url,$credentials->user,$credentials->pwd ,$debug);
 $secs = 15;
 $fn = $params[0] ;
     
